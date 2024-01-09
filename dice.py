a3ef1964993aef8d66ebbd513a388daf770e5930
mpkmorgan <mpkuloba01@gmail.com>
2024-01-09 04:41:40 +0300

    Update dice.py
    Bug fixed which had capped the max number of players at 4.
    
    Able to display cumulative scores

--- dice.py
+++ dice.py
@@ -21,3 +21,29 @@ while True:
 max_score = 50
 players_score = [0 for _ in range (players)] 
 print(players_score)
+
+while max(players_score) < max_score:
+   
+        for player_indx in range(players): 
+         print("\nplayer",player_indx + 1,"turn has just started!\n")
+        current_score = 0
+        
+        while True:
+            should_toss = input("Would you like to toss (y)? ")
+            if should_toss.lower() !="y":
+                break
+
+            value = toss()
+            if value == 1:
+                print("You tossed a 1! Turn done!")
+                break
+
+            else:
+             current_score += value
+            print("You tossed a:", value)
+
+            print("Your current score is:" , current_score)
+        players_score[player_indx] += current_score
+        print("Total score is:",players_score[player_indx])
+
+
